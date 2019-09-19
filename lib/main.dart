b003693cff2c0f861e657e4b38fc5dcc3e0e17f6
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-09-18 22:12:43 -0500

    Disabled the debug banner.

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'James\s Anxiety tracking app.',
       theme: ThemeData(
         primarySwatch: Colors.blue,
