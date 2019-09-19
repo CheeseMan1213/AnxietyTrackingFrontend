93cfa487c85936e7d0a2c981f8097e3405b3014b
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-09-18 22:18:03 -0500

    Converted to a StatefulWidget, added import for table calendar, added code for the calendar, and pass arguments: 'Hi, I am James.'To the AnxietyEntryPage route in pushNamed()

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,11 +23,29 @@ class CalendarPage extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Text('Routing 2'),
+          TableCalendar(
+            calendarStyle: CalendarStyle(
+              todayColor: Theme.of(context).primaryColorDark,
+              selectedColor: Theme.of(context).primaryColor,
+            ),
+            calendarController: _controller,
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            onDaySelected: (date, events) {
+              //print('You are so cool!');
+              Navigator.of(context).pushNamed(
+                '/anxiety_entry',
+                arguments: 'Hi, I am James.',
+              );
+            },
+          ),
+          Text('Calendar'),
           RaisedButton(
             child: Text('Anxiety Entry'),
             onPressed: () {
-              Navigator.of(context).pushNamed('/anxiety_entry');
+              Navigator.of(context).pushNamed(
+                '/anxiety_entry',
+                arguments: 'Hi, I am James.',
+              );
             },
           ),
         ],
