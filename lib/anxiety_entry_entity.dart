e45adcf7cbd4cc7996ceadf77996eeb656c19fb5
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-09-25 14:22:34 -0500

    Removed the final property form each member variable. Added setters and getters.

--- lib/anxiety_entry_entity.dart
+++ lib/anxiety_entry_entity.dart
@@ -1,9 +1,29 @@
 import 'package:anxiety_tracking_front_end/today_was.dart';
 
 class AnxietyTrackingEntity {
-  final DateTime _date;
-  final String _anxEntry;
-  final TodayWas _todayWas;
+  DateTime _date;
+  String _anxEntry;
+  TodayWas _todayWas;
 
   AnxietyTrackingEntity(this._date, this._anxEntry, this._todayWas);
+
+  DateTime getDate() {
+    return _date;
+  }
+  String getAnxEntry() {
+    return _anxEntry;
+  }
+  TodayWas getTodayWas() {
+    return _todayWas;
+  }
+  void setDate(date) {
+    this._date = date;
+  }
+  void setAnxEntry(anxEntry) {
+    this._anxEntry = anxEntry;
+  }
+  void setTodayWas(todayWas) {
+    this._todayWas = todayWas;
+  }
+
 }
