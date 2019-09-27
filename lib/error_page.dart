f9a5e1a8918baa384bce4e61d44ad81ce464958d
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-09-27 13:34:10 -0500

    Added comments.

--- lib/error_page.dart
+++ lib/error_page.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
+///This widget gets displayed if you try to navigate to an incorrect route.
 class ErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Error Page.'),
+      child: Text(
+        'Error Page.',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 36.0,
+        ),
+      ),
     );
   }
 }
