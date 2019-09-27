e3d2263a3cd1fb1611d8d845086194ee01d97af2
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-09-27 13:34:27 -0500

    Added comments.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import './route_generator.dart';
 
+///This is the main entry point of the whole app.
 void main() => runApp(MyApp());
 
+///This is the first class that gets ran from the app entry point.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,//removes the debug banner.
       title: 'James\s Anxiety tracking app.',
       theme: ThemeData(
         primarySwatch: Colors.blue,
