a6371aaecce50367c7962215d2c3fae458ae6f7e
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-09-27 13:33:55 -0500

    Added comments.

--- lib/anxiety_entry_entity.dart
+++ lib/anxiety_entry_entity.dart
@@ -1,12 +1,18 @@
 import 'package:anxiety_tracking_front_end/today_was.dart';
 
+///This is my entity(for MySQL people)/collection(for MongoDB people)
+///for the anxiety entry.
 class AnxietyTrackingEntity {
-  DateTime _date;
-  String _anxEntry;
-  TodayWas _todayWas;
 
+  //Member variables.
+  DateTime _date;//Date that will be passed form the calendar page.
+  String _anxEntry;//The anxiety entry text.
+  TodayWas _todayWas;//For the radio buttons.
+
+  //Constructor.
   AnxietyTrackingEntity(this._date, this._anxEntry, this._todayWas);
 
+  //Getters and setters.
   DateTime getDate() {
     return _date;
   }
