9b15f19f180be5944029c49b324e5950184580c3
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-16 10:34:03 -0500

    Added works cited, removed/added comments, began to use the .then() method on the getAnxietyByDate() call in initState(). Added a if statement and a loading screen above the actual page in the build method. Stopped using _anxEntry variable and replaced it use with more direct calls. Added verification for the user to pick a todayWas radio button. Made the onPressed() of the submit button async, and also add an if/else statement to it to post or update. Modified method selectThisRadioButton() to return a String, and set state differently. Modified getAnxietyByDate() to return a Future instead of a Future<String>, to return responses instead of those never used Strings, and have it set the radio button too. Deleted method asyncDataTextWidget().

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -1,3 +1,9 @@
+/***
+ * Work Cited:
+ * https://flutter.institute/run-async-operation-on-widget-creation/
+ */
+
+
 import 'dart:async';
 import 'dart:convert';
 
@@ -10,7 +16,6 @@ import 'api_services/anxiety_entity_service/anxiety_service.dart';
 
 ///This is the anxiety entry route. It take a date for the previous route, the calender page.
 ///It is a stateful widget.
-///
 class AnxietyEntryPage extends StatefulWidget {
   final DateTime date;
 
@@ -21,24 +26,59 @@ class AnxietyEntryPage extends StatefulWidget {
 }
 
 class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
-  Anxiety _anxEntry = Anxiety(null, 'No entry given.', TodayWas.NotSelectedYet.toString());
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _anxEntryController = TextEditingController();
   int groupValue;
-  List<dynamic> _data;
+  var _data;
 
   @override
   void initState() {
-    _anxEntry.setDate(widget.date.toIso8601String());
-    this.getAnxietyByDate('http://localhost:60000/anxieties/' + _anxEntry.getDate());
+    this.getAnxietyByDate('http://localhost:60000/anxieties/' + widget.date.toIso8601String()).then((data) {
+      setState(() {
+        this._data = data;
+      });
+    });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     //This page gets the date the user chose from the previous page.
-    //_anxEntry.setDate(widget.date.toIso8601String());
 
+    //check for before the async call has returned. This is the loading screen.
+    if(_data == null) {
+      return Scaffold(
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomRight,
+                stops: [0.1, 0.5, 0.7, 0.9],
+                colors: [
+                  Colors.green[800],
+                  Colors.green[600],
+                  Colors.green[400],
+                  Colors.green[200],
+                ],
+                //tileMode: TileMode.clamp
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'Loading....',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 36,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Anxiety Entry'),
@@ -78,7 +118,7 @@ class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
                           Container(
                             color: Colors.white,
                             child: Text(
-                              displayDate(_anxEntry.getDate()) + ':',
+                              displayDate(widget.date.toIso8601String()) + ':',
                               style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 24.0,
@@ -166,6 +206,7 @@ class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
                         ],
                       ),
                       TextFormField(
+                        //The controller adds additional functionality and data retrieval to the TextFormField()
                         controller: this._anxEntryController,
                         keyboardType: TextInputType.multiline,
                         cursorColor: Colors.white,
@@ -185,7 +226,6 @@ class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
                               width: 1.0,
                             ),
                           ),
-
                           labelStyle: TextStyle(
                             color: Colors.white,
                           ),
@@ -201,15 +241,17 @@ class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
                           if (value.isEmpty) {
                             return 'Please make you anxiety entry.';
                           }
+                          if(this.groupValue == null || this.groupValue == 0) {
+                            return 'Please select how your day was.';
+                          }
                           return null;
                         },
                         onSaved: (val) {
                           setState(() {
-                            _anxEntry.setAnxEntry(val);
+                            //_anxEntry.setAnxEntry(val);
+                            //_data[0].setAnxEntry(val);
                           });
                         },
-                        //onSaved: (val) => setState()
-                        //setState(() => anxEntry.setAnxEntry(val.toString())),
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
@@ -225,26 +267,40 @@ class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
                               ),
                             ),
                             splashColor: Colors.redAccent,
-                            onPressed: () {
+                            onPressed: () async {
                               final form = _formKey.currentState;
                               if (form.validate()) {
                                 form.save();//save data to local variables.
-                                //AnxietyService.postAnxiety('http://localhost:60000/anxieties', this._data[0].getDate(), this._data[0].getAnxEntry(), this._data[0].getTodayWas().toString());
-                                AnxietyService.postAnxiety('http://localhost:60000/anxieties', _anxEntry.getDate(), _anxEntry.getAnxEntry(), _anxEntry.getTodayWas().toString());
+                                if(this._data == null || this._data.isEmpty) {
+                                  await AnxietyService.postAnxiety('http://localhost:60000/anxieties', widget.date.toIso8601String(), _anxEntryController.text, selectThisRadioButton(this.groupValue));
+                                  print("Did a post.");
+                                  Scaffold.of(context).showSnackBar(SnackBar(content: Text('Created new entry.')));
+                                  //This line fixes a bug where if the user picks a date with no entry in the database,
+                                  // it will continue to create new entries, instead of create once, then update.
+                                  await this.getAnxietyByDate('http://localhost:60000/anxieties/' + widget.date.toIso8601String()).then((data) {
+                                    setState(() {
+                                      this._data = data;
+                                    });
+                                  });
+                                }
+                                else {
+                                  this._data[0].setAnxEntry(_anxEntryController.text);
+                                  this._data[0].setTodayWas(selectThisRadioButton(this.groupValue).toString());
+                                  await AnxietyService.putAnxiety('http://localhost:60000/anxieties', this._data[0]);
+                                  print("Did a put.");
+                                  Scaffold.of(context).showSnackBar(SnackBar(content: Text('Updated this entry.')));
+                                  print(this._data);
+                                }
                                 FocusScope.of(context).requestFocus(FocusNode());
+                                setState(() {
+                                  //
+                                });
                               }
-                              Scaffold.of(context).showSnackBar(
-                                  SnackBar(content: Text('Processing Data')));
                             },
                             child: Text('Submit'),
                           ),
                         ],
                       ),
-                      //Data to later save to database.
-                      Text(_anxEntry.getAnxEntry()),
-                      Text(displayDate(_anxEntry.getDate())),
-                      Text(_anxEntry.getTodayWas().toString()),
-                      //asyncDataTextWidget(),
                       RaisedButton(
                         child: Text('Return to homepage'),
                         onPressed: () {
@@ -261,41 +317,43 @@ class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
       ),
     );
   }
-///This function is used by the radio buttons. It is how the "change which one is selected"
+  ///This function is used by the radio buttons. It is how the "change which one is selected"
   ///functionality works. When the 'group value' and the 'value' are the same, then that radio
-  ///button get selected.
-  void selectThisRadioButton(int e) {
-    setState(() {
-      if (e == 1) {
+  ///button get selected. This function also returns a string for what 'todayWas' is for use by the post method.
+  String selectThisRadioButton(int e) {
+
+    switch (e) {
+      case 1:
         this.groupValue = 1;
-      }
-      switch (e) {
-        case 1:
-          this.groupValue = 1;
-          this._anxEntry.setTodayWas(TodayWas.Awesome.toString());
-          break;
-        case 2:
-          this.groupValue = 2;
-          this._anxEntry.setTodayWas(TodayWas.Good.toString());
-          break;
-        case 3:
-          this.groupValue = 3;
-          this._anxEntry.setTodayWas(TodayWas.Fine.toString());
-          break;
-        case 4:
-          this.groupValue = 4;
-          this._anxEntry.setTodayWas(TodayWas.NotSoGood.toString());
-          break;
-        case 5:
-          this.groupValue = 5;
-          this._anxEntry.setTodayWas(TodayWas.Terrible.toString());
-          break;
-        default:
-          this.groupValue = 0;
-          this._anxEntry.setTodayWas(TodayWas.NotSelectedYet.toString());
-          break;
-      }
-    });
+        setState(() {});
+        return TodayWas.Awesome.toString();
+        break;
+      case 2:
+        this.groupValue = 2;
+        setState(() {});
+        return TodayWas.Good.toString();
+        break;
+      case 3:
+        this.groupValue = 3;
+        setState(() {});
+        return TodayWas.Fine.toString();
+        break;
+      case 4:
+        this.groupValue = 4;
+        setState(() {});
+        return TodayWas.NotSoGood.toString();
+        break;
+      case 5:
+        this.groupValue = 5;
+        setState(() {});
+        return TodayWas.Terrible.toString();
+        break;
+      default:
+        this.groupValue = 0;
+        setState(() {});
+        return TodayWas.NotSelectedYet.toString();
+        break;
+    }
   }
   ///Take the DateTime object and displays only the date
   ///in month/day/year format.
@@ -307,44 +365,36 @@ class _AnxietyEntryPageState extends State<AnxietyEntryPage> {
         '/' +
         castedDate.year.toString();
   }
-  Future<String> getAnxietyByDate(url) async {
+  ///Takes a URL with a date added to the end, and sends it off to get one object back from the database.
+  Future getAnxietyByDate(url) async {
     var response  = await http.get(
       //Encode the url
         Uri.encodeFull(url),
         //only accept json response
         headers: {"Accept": "application/json"});
     int statusCode = response.statusCode;
-
+    //Checks status code.
     if (statusCode == 200) {
       print('Successful Get.');
       var convertDataToJson2 = json.decode(response.body);
-      List<dynamic> responses = convertDataToJson2
-          .map((j) => Anxiety.fromJson(j))
-          .toList();
-      this._data = responses;
-      if(this._data == null || this._data.isEmpty) {
+      var responses = convertDataToJson2.map((j) => Anxiety.fromJson(j)).toList();
+      if(responses == null || responses.isEmpty) {
         _anxEntryController.text = "No entry for this date.";
       }
       else {
-        this._anxEntryController.text = _data[0].getAnxEntry();
-        selectThisRadioButton(this._data[0].getTodayWasAsInteger());
-        //final TodayWas todayWas = _data[0].getTodayWas();
+        //Populates text for field with date from database if it is there.
+        this._anxEntryController.text = responses[0].getAnxEntry();
+        //sets state of radio button from database.
+        selectThisRadioButton(responses[0].getTodayWasAsInteger());
       }
       setState(() {
         //
       });
-      return 'Successful Get.';
+      return responses;
     } else {
       print('Failed Get. Status code: ' + statusCode.toString());
       print(url);
-      return 'Failed Get.';
-    }
-  }
-  Text asyncDataTextWidget() {
-    if(this._data == null){
-    return Text('LOADING.....');
-    } else {
-    return Text(this._data[0]['firstName'] +' '+ this._data[0]['lastName'] +' '+ this._data[0]['ttime']);
+      return null;
     }
   }
 }
