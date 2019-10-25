c17d40ff0c29c4dfaccfab5dc1cb48cfde86b547
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-25 15:32:23 -0500

    Added more imports, made _date final, began using the StaticServerIP class, wrapped the raised button in a ScopedModelDescendant, added line "await model.getData(this._date);"

--- lib/my_widgets/delete_button.dart
+++ lib/my_widgets/delete_button.dart
@@ -1,36 +1,41 @@
 import 'package:flutter/material.dart';
 import '../api_services/anxiety_entity_service/anxiety_service.dart';
+import '../static_server_ip/static_server_ip.dart';
+import 'package:scoped_model/scoped_model.dart';
+import '../models/anxiety_model/scoped_anxiety.dart';
 
 class DeleteButton extends StatelessWidget {
-  String _date;
+  final String _date;
   var _data;
-  String serverIP;
+  static const String serverIP = StaticServerIP.serverIP;
 
-  DeleteButton(this._date, this._data, this.serverIP);
+  DeleteButton(this._date, this._data);
 
   @override
   Widget build(BuildContext context) {
-
-    return RaisedButton(
-      color: Colors.white,
-      textColor: Colors.black,
-      shape: RoundedRectangleBorder(
-        side: BorderSide(
-            style: BorderStyle.solid,
-            width: 5.0,
-            color: Colors.purple
-        ),
-      ),
-      splashColor: Colors.redAccent,
-      onPressed: () async {
-        if(this._data != null && this._data.isNotEmpty) {
-          await AnxietyService.deleteAnxiety('http://' + serverIP + ':60000/deleteAnxiety', this._data[0]);
-          print("Did a delete.");
-          Scaffold.of(context).showSnackBar(SnackBar(content: Text('Deleted this entry.')));
-        }
-        FocusScope.of(context).requestFocus(FocusNode());
-      },
-      child: Text('Delete'),
-    );
+    return ScopedModelDescendant<ScopedAnxiety>(
+        builder: (context, child, model) =>
+            RaisedButton(
+              color: Colors.white,
+              textColor: Colors.black,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                    style: BorderStyle.solid,
+                    width: 5.0,
+                    color: Colors.purple
+                ),
+              ),
+              splashColor: Colors.redAccent,
+              onPressed: () async {
+                if (this._data != null && this._data.isNotEmpty) {
+                  await AnxietyService.deleteAnxiety('http://' + serverIP + ':60000/deleteAnxiety', this._data[0]);
+                  await model.getData(this._date);
+                  print("Did a delete.");
+                  Scaffold.of(context).showSnackBar(SnackBar(content: Text('Deleted this entry.')));
+                }
+                FocusScope.of(context).requestFocus(FocusNode());
+              },
+              child: Text('Delete'),
+            ));
   }
 }
