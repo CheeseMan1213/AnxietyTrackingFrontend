88abf61f9565efdd687e2e880531f659092f6ae0
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-24 19:53:50 -0500

    Adjusted imports, uncommented the whole class, started using variables serverIP and _data

--- lib/my_widgets/delete_button.dart
+++ lib/my_widgets/delete_button.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import '../api_services/anxiety_entity_service/anxiety_service.dart';
 
-/*class DeleteButton extends StatelessWidget {
+class DeleteButton extends StatelessWidget {
   String _date;
+  var _data;
+  String serverIP;
 
-  DeleteButton(this._date);
+  DeleteButton(this._date, this._data, this.serverIP);
 
   @override
   Widget build(BuildContext context) {
+
     return RaisedButton(
       color: Colors.white,
       textColor: Colors.black,
@@ -23,53 +24,13 @@ import 'package:http/http.dart' as http;
       splashColor: Colors.redAccent,
       onPressed: () async {
         if(this._data != null && this._data.isNotEmpty) {
-          await AnxietyService.deleteAnxiety('http://localhost:60000/deleteAnxiety', this._data[0]);
+          await AnxietyService.deleteAnxiety('http://' + serverIP + ':60000/deleteAnxiety', this._data[0]);
           print("Did a delete.");
           Scaffold.of(context).showSnackBar(SnackBar(content: Text('Deleted this entry.')));
-          await this.getAnxietyByDate('http://localhost:60000/anxieties/' + _date).then((data) {
-            setState(() {
-              this._data = data;
-            });
-          });
         }
         FocusScope.of(context).requestFocus(FocusNode());
-        setState(() {
-          //
-        });
       },
       child: Text('Delete'),
     );
   }
-  ///Takes a URL with a date added to the end, and sends it off to get one object back from the database.
-  Future getAnxietyByDate(url) async {
-    var response  = await http.get(
-      //Encode the url
-        Uri.encodeFull(url),
-        //only accept json response
-        headers: {"Accept": "application/json"});
-    int statusCode = response.statusCode;
-    //Checks status code.
-    if (statusCode == 200) {
-      print('Successful Get.');
-      var convertDataToJson2 = json.decode(response.body);
-      var responses = convertDataToJson2.map((j) => Anxiety.fromJson(j)).toList();
-      if(responses == null || responses.isEmpty) {
-        _anxEntryController.text = "No entry for this date.";
-      }
-      else {
-        //Populates text for field with date from database if it is there.
-        this._anxEntryController.text = responses[0].getAnxEntry();
-        //sets state of radio button from database.
-        selectThisRadioButton(responses[0].getTodayWasAsInteger());
-      }
-      setState(() {
-        //
-      });
-      return responses;
-    } else {
-      print('Failed Get. Status code: ' + statusCode.toString());
-      print(url);
-      return null;
-    }
-  }
-}*/
+}
