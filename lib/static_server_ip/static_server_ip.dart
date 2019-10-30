bc8d4a528bf2a252a1ec007d2310f44beff4a508
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-30 12:45:18 -0500

    Added port to address.

--- lib/static_server_ip/static_server_ip.dart
+++ lib/static_server_ip/static_server_ip.dart
@@ -1,4 +1,4 @@
 class StaticServerIP {
-  //static const String serverIP = "192.168.1.14";
-  static const String serverIP = "localhost";
+  //static const String serverIP = "192.168.1.14:60000";
+  static const String serverIP = "localhost:60000";
 }
