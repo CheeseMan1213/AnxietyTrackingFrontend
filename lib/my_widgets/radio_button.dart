e74e96c6b2e288a7fd7e6b500563289f4a613b13
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-31 15:29:58 -0500

    Started using OtherMethods.selectThisRadioButton(), and deleted selectThisRadioButton() from this file, because it in in a service method now.

--- lib/my_widgets/radio_button.dart
+++ lib/my_widgets/radio_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import '../models/anxiety_model/scoped_anxiety.dart';
-import '../today_was.dart';
+import '../api_services/anxiety_entity_service/other_methods.dart';
 
 class RadioButton extends StatelessWidget {
   final int value;
@@ -13,52 +13,11 @@ class RadioButton extends StatelessWidget {
     return ScopedModelDescendant<ScopedAnxiety>(
         builder: (context, child, model) =>
             Radio(
-              onChanged: (int e) => selectThisRadioButton(e, model),
+              onChanged: (int e) => OtherMethods.selectThisRadioButton(e, model),
               activeColor: Colors.black,
               value: this.value,// When matches 'groupValue' the radio button gets selected.
               groupValue: model.anxietyBabyClass.groupValue,
             ),
     );
-  }
-  String selectThisRadioButton(int e, ScopedAnxiety model) {
-
-    switch (e) {
-      case 1:
-        model.updateGradientColor(1);
-        //this.groupValue = 1;
-        //setState(() {});
-        return TodayWas.Awesome.toString();
-        break;
-      case 2:
-        model.updateGradientColor(2);
-        //this.groupValue = 2;
-        //setState(() {});
-        return TodayWas.Good.toString();
-        break;
-      case 3:
-        model.updateGradientColor(3);
-        //this.groupValue = 3;
-        //setState(() {});
-        return TodayWas.Fine.toString();
-        break;
-      case 4:
-        model.updateGradientColor(4);
-        //this.groupValue = 4;
-        //setState(() {});
-        return TodayWas.NotSoGood.toString();
-        break;
-      case 5:
-        model.updateGradientColor(5);
-        //this.groupValue = 5;
-        //setState(() {});
-        return TodayWas.Terrible.toString();
-        break;
-      default:
-        model.updateGradientColor(0);
-        //this.groupValue = 0;
-        //setState(() {});
-        return TodayWas.NotSelectedYet.toString();
-        break;
-    }
   }
 }
--- lib/my_widgets/submit_button.dart
+++ lib/my_widgets/submit_button.dart
@@ -3,7 +3,7 @@ import '../api_services/anxiety_entity_service/anxiety_service.dart';
 import '../static_server_ip/static_server_ip.dart';
 import 'package:scoped_model/scoped_model.dart';
 import '../models/anxiety_model/scoped_anxiety.dart';
-import '../today_was.dart';
+import '../api_services/anxiety_entity_service/other_methods.dart';
 
 class SubmitButton extends StatelessWidget {
   final GlobalKey<FormState> _formKey;
@@ -34,7 +34,7 @@ class SubmitButton extends StatelessWidget {
                   form.save();//save data to local variables.
                   //If there is no entry, add a new one once.
                   if(model.anxietyBabyClass.data == null || model.anxietyBabyClass.data.isEmpty) {
-                    await AnxietyService.postAnxiety('http://' + serverIP + '/anxieties', _date.toIso8601String(), _anxEntryController.text, selectThisRadioButton(model.anxietyBabyClass.groupValue, model));
+                    await AnxietyService.postAnxiety('http://' + serverIP + '/anxieties', _date.toIso8601String(), _anxEntryController.text, OtherMethods.selectThisRadioButton(model.anxietyBabyClass.groupValue, model));
                     await model.getData(this._date.toIso8601String());
                     print("Did a post.");
                     Scaffold.of(context).showSnackBar(SnackBar(duration: const Duration(seconds: 1), content: Text('Created new entry.')));
@@ -56,7 +56,7 @@ class SubmitButton extends StatelessWidget {
                       //Populates text for field with date from database if it is there.
                       _anxEntryController.text = model.anxietyBabyClass.data[0].getAnxEntry();
                       //sets state of radio button from database.
-                      selectThisRadioButton(model.anxietyBabyClass.data[0].getTodayWasAsInteger(), model);
+                      OtherMethods.selectThisRadioButton(model.anxietyBabyClass.data[0].getTodayWasAsInteger(), model);
                       /*setState(() {
                                         //
                                       });*/
@@ -65,7 +65,7 @@ class SubmitButton extends StatelessWidget {
                   //else update the existing one.
                   else {
                     model.anxietyBabyClass.data[0].setAnxEntry(_anxEntryController.text);
-                    model.anxietyBabyClass.data[0].setTodayWas(selectThisRadioButton(model.anxietyBabyClass.groupValue, model).toString());
+                    model.anxietyBabyClass.data[0].setTodayWas(OtherMethods.selectThisRadioButton(model.anxietyBabyClass.groupValue, model).toString());
                     await AnxietyService.putAnxiety('http://' + serverIP + '/anxieties', model.anxietyBabyClass.data[0]);
                     await model.getData(this._date.toIso8601String());
                     print("Did a put.");
@@ -80,46 +80,5 @@ class SubmitButton extends StatelessWidget {
               },
               child: Text('Submit'),
             ));
-  }
-  String selectThisRadioButton(int e, ScopedAnxiety model) {
-
-    switch (e) {
-      case 1:
-        model.updateGradientColor(1);
-        //this.groupValue = 1;
-        //setState(() {});
-        return TodayWas.Awesome.toString();
-        break;
-      case 2:
-        model.updateGradientColor(2);
-        //this.groupValue = 2;
-        //setState(() {});
-        return TodayWas.Good.toString();
-        break;
-      case 3:
-        model.updateGradientColor(3);
-        //this.groupValue = 3;
-        //setState(() {});
-        return TodayWas.Fine.toString();
-        break;
-      case 4:
-        model.updateGradientColor(4);
-        //this.groupValue = 4;
-        //setState(() {});
-        return TodayWas.NotSoGood.toString();
-        break;
-      case 5:
-        model.updateGradientColor(5);
-        //this.groupValue = 5;
-        //setState(() {});
-        return TodayWas.Terrible.toString();
-        break;
-      default:
-        model.updateGradientColor(0);
-        //this.groupValue = 0;
-        //setState(() {});
-        return TodayWas.NotSelectedYet.toString();
-        break;
-    }
   }
 }
