7f221d10f45b34e2603f2f9c1a3d3d8ee9204d2d
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-10-31 15:28:34 -0500

    Removed unused imports, added import for other_methods.dart, delete the rest of the commented out setState() methods, began using OtherMethods.selectThisRadioButton() and deleted selectThisRadioButton() from being in this file. Deleted the commented out inline code that is now done in the custom widgets.

--- lib/third_page.dart
+++ lib/third_page.dart
@@ -3,10 +3,8 @@
  * https://flutter.institute/run-async-operation-on-widget-creation/
  */
 
-import 'package:anxiety_tracking_front_end/today_was.dart';
 import 'package:flutter/material.dart';
 
-import 'api_services/anxiety_entity_service/anxiety_service.dart';
 import 'my_widgets/delete_button.dart';
 import 'my_widgets/radio_button.dart';
 import 'my_widgets/submit_button.dart';
@@ -14,8 +12,9 @@ import 'my_widgets/anxiety_entry_form.dart';
 
 import 'static_server_ip/static_server_ip.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'my_widgets/loading_screen.dart';
+
 import 'models/anxiety_model/scoped_anxiety.dart';
+import 'api_services/anxiety_entity_service/other_methods.dart';
 
 ///This is the anxiety entry route. It take a date for the previous route, the calender page.
 ///It is a stateful widget.
@@ -46,18 +45,12 @@ class AnxietyEntryPage extends StatelessWidget {
         //print("From loading if statement = " + scopedAnxiety.anxietyBabyClass.data.toString());
         if(scopedAnxiety.anxietyBabyClass.data == null || scopedAnxiety.anxietyBabyClass.data.isEmpty) {
           _anxEntryController.text = "";//No entry yet.
-          /*setState(() {
-            //
-          });*/
         }
         else {
           //Populates text for field with date from database if it is there.
           _anxEntryController.text = scopedAnxiety.anxietyBabyClass.data[0].getAnxEntry();
           //sets state of radio button from database.
-          selectThisRadioButton(scopedAnxiety.anxietyBabyClass.data[0].getTodayWasAsInteger());
-          /*setState(() {
-            //
-          });*/
+          OtherMethods.selectThisRadioButton(scopedAnxiety.anxietyBabyClass.data[0].getTodayWasAsInteger(), scopedAnxiety);
         }
       });
       //return LoadingScreen();
@@ -123,31 +116,10 @@ class AnxietyEntryPage extends StatelessWidget {
                                   // Row 1
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: <Widget>[
-                                    /*Radio(
-                                      // For Awesome
-                                      onChanged: (int e) => selectThisRadioButton(e),
-                                      activeColor: Colors.black,
-                                      value: 1,// When matches 'groupValue' the radio button gets selected.
-                                      groupValue: scopedAnxiety.anxietyBabyClass.groupValue,
-                                    ),*/
                                     RadioButton(1),
                                     Text('Awesome'),
-                                    /*Radio(
-                                      // For Good
-                                      onChanged: (int e) => selectThisRadioButton(e),
-                                      activeColor: Colors.black,
-                                      value: 2,// When matches 'groupValue' the radio button gets selected.
-                                      groupValue: scopedAnxiety.anxietyBabyClass.groupValue,
-                                    ),*/
                                     RadioButton(2),
                                     Text('Good'),
-                                    /*Radio(
-                                      // For Fine
-                                      onChanged: (int e) => selectThisRadioButton(e),
-                                      activeColor: Colors.black,
-                                      value: 3,// When matches 'groupValue' the radio button gets selected.
-                                      groupValue: scopedAnxiety.anxietyBabyClass.groupValue,
-                                    ),*/
                                     RadioButton(3),
                                     Text('Fine'),
                                   ],
@@ -156,22 +128,8 @@ class AnxietyEntryPage extends StatelessWidget {
                                   // Row 2
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: <Widget>[
-                                    /*Radio(
-                                      // For NotSoGood
-                                      onChanged: (int e) => selectThisRadioButton(e),
-                                      activeColor: Colors.black,
-                                      value: 4,// When matches 'groupValue' the radio button gets selected.
-                                      groupValue: scopedAnxiety.anxietyBabyClass.groupValue,
-                                    ),*/
                                     RadioButton(4),
                                     Text('Not so good'),
-                                    /*Radio(
-                                      // For Terrible
-                                      onChanged: (int e) => selectThisRadioButton(e),
-                                      activeColor: Colors.black,
-                                      value: 5,// When matches 'groupValue' the radio button gets selected.
-                                      groupValue: scopedAnxiety.anxietyBabyClass.groupValue,
-                                    ),*/
                                     RadioButton(5),
                                     Text('Terrible'),
                                   ],
@@ -188,123 +146,11 @@ class AnxietyEntryPage extends StatelessWidget {
                                   ],
                                 ),
                                 AnxietyEntryForm(_anxEntryController),
-                                /*TextFormField(
-                                  //The controller adds additional functionality and data retrieval to the TextFormField()
-                                  controller: this._anxEntryController,
-                                  keyboardType: TextInputType.multiline,
-                                  cursorColor: Colors.white,
-                                  autocorrect: true,
-                                  textCapitalization: TextCapitalization.sentences,
-                                  maxLines: 5,
-                                  maxLength: 194,
-                                  maxLengthEnforced: true,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                  decoration: InputDecoration(
-                                    labelText: 'Anxiety Entry:',
-                                    hintText: "No entry yet.",
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Colors.white,
-                                        width: 1.0,
-                                      ),
-                                    ),
-                                    labelStyle: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                    border: OutlineInputBorder(),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Colors.white,
-                                        width: 1.0,
-                                      ),
-                                    ),
-                                  ),
-                                  validator: (value) {
-                                    if (value.isEmpty) {
-                                      return 'Please make you anxiety entry.';
-                                    }
-                                    if(scopedAnxiety.anxietyBabyClass.groupValue == null || scopedAnxiety.anxietyBabyClass.groupValue == 0) {
-                                      return 'Please select how your day was.';
-                                    }
-                                    return null;
-                                  },
-                                  onSaved: (val) {
-                                    *//*setState(() {
-                              _anxEntry.setAnxEntry(val);
-                              _data[0].setAnxEntry(val);
-                            });*//*
-                                  },
-                                ),*/
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.end,
                                   children: <Widget>[
                                     DeleteButton(date.toIso8601String()),
                                     SubmitButton(date, _formKey, _anxEntryController),
-                                    /*RaisedButton(
-                                      color: Colors.white,
-                                      textColor: Colors.black,
-                                      shape: RoundedRectangleBorder(
-                                        side: BorderSide(
-                                            style: BorderStyle.solid,
-                                            width: 5.0,
-                                            color: Colors.purple
-                                        ),
-                                      ),
-                                      splashColor: Colors.redAccent,
-                                      onPressed: () async {
-                                        final form = _formKey.currentState;
-                                        if (form.validate()) {
-                                          form.save();//save data to local variables.
-                                          //If there is no entry, add a new one once.
-                                          if(model.anxietyBabyClass.data == null || model.anxietyBabyClass.data.isEmpty) {
-                                            await AnxietyService.postAnxiety('http://' + serverIP + '/anxieties', date.toIso8601String(), _anxEntryController.text, selectThisRadioButton(scopedAnxiety.anxietyBabyClass.groupValue));
-                                            await model.getData(this.date.toIso8601String());
-                                            print("Did a post.");
-                                            Scaffold.of(context).showSnackBar(SnackBar(duration: const Duration(seconds: 1), content: Text('Created new entry.')));
-                                            //This line fixes a bug where if the user picks a date with no entry in the database,
-                                            // it will continue to create new entries, instead of create once, then update.
-                                            *//*await AnxietyService.getAnxietyByDate('http://' + serverIP + '/anxieties/' + date.toIso8601String()).then((data) {
-                                      setState(() {
-                                        this._data = data;
-                                      });
-                                    });*//*
-                                            //if it is still null or empty after the post.
-                                            if(model.anxietyBabyClass.data == null || model.anxietyBabyClass.data.isEmpty) {
-                                              _anxEntryController.text = "There was an error with the post.";
-                                              *//*setState(() {
-                                        //
-                                      });*//*
-                                            }
-                                            else {
-                                              //Populates text for field with date from database if it is there.
-                                              _anxEntryController.text = model.anxietyBabyClass.data[0].getAnxEntry();
-                                              //sets state of radio button from database.
-                                              selectThisRadioButton(model.anxietyBabyClass.data[0].getTodayWasAsInteger());
-                                              *//*setState(() {
-                                        //
-                                      });*//*
-                                            }
-                                          }
-                                          //else update the existing one.
-                                          else {
-                                            model.anxietyBabyClass.data[0].setAnxEntry(_anxEntryController.text);
-                                            model.anxietyBabyClass.data[0].setTodayWas(selectThisRadioButton(scopedAnxiety.anxietyBabyClass.groupValue).toString());
-                                            await AnxietyService.putAnxiety('http://' + serverIP + '/anxieties', model.anxietyBabyClass.data[0]);
-                                            await model.getData(this.date.toIso8601String());
-                                            print("Did a put.");
-                                            Scaffold.of(context).showSnackBar(SnackBar(duration: const Duration(seconds: 1), content: Text('Updated this entry.')));
-                                            print(model.anxietyBabyClass.data);
-                                          }
-                                          FocusScope.of(context).requestFocus(FocusNode());
-                                          *//*setState(() {
-                                    //
-                                  });*//*
-                                        }
-                                      },
-                                      child: Text('Submit'),
-                                    ),*/
                                   ],
                                 ),
                                 RaisedButton(
@@ -326,50 +172,6 @@ class AnxietyEntryPage extends StatelessWidget {
       );
     }
 
-  }
-  ///This function is used by the radio buttons. It is how the "change which one is selected"
-  ///functionality works. When the 'group value' and the 'value' are the same, then that radio
-  ///button get selected. This function also returns a string for what 'todayWas' is for use by the post method.
-  String selectThisRadioButton(int e) {
-
-    switch (e) {
-      case 1:
-        scopedAnxiety.updateGradientColor(1);
-        //this.groupValue = 1;
-        //setState(() {});
-        return TodayWas.Awesome.toString();
-        break;
-      case 2:
-        scopedAnxiety.updateGradientColor(2);
-        //this.groupValue = 2;
-        //setState(() {});
-        return TodayWas.Good.toString();
-        break;
-      case 3:
-        scopedAnxiety.updateGradientColor(3);
-        //this.groupValue = 3;
-        //setState(() {});
-        return TodayWas.Fine.toString();
-        break;
-      case 4:
-        scopedAnxiety.updateGradientColor(4);
-        //this.groupValue = 4;
-        //setState(() {});
-        return TodayWas.NotSoGood.toString();
-        break;
-      case 5:
-        scopedAnxiety.updateGradientColor(5);
-        //this.groupValue = 5;
-        //setState(() {});
-        return TodayWas.Terrible.toString();
-        break;
-      default:
-        scopedAnxiety.updateGradientColor(0);
-        //this.groupValue = 0;
-        //setState(() {});
-        return TodayWas.NotSelectedYet.toString();
-        break;
-    }
   }
   ///Take the DateTime object and displays only the date
   ///in month/day/year format.
