f62545f71f2ef45a0edf258094ab834d361e59cc
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-11-01 17:01:23 -0500

    Changed return type of method selectThisRadioButton() from String to TodayWas, and updated the return values accordingly.

--- lib/api_services/anxiety_entity_service/other_methods.dart
+++ lib/api_services/anxiety_entity_service/other_methods.dart
@@ -6,32 +6,32 @@ class OtherMethods {
   ///This function is used by the radio buttons. It is how the "change which one is selected"
   ///functionality works. When the 'group value' and the 'value' are the same, then that radio
   ///button gets selected. This function also returns a string for what 'todayWas' is for use by the post method.
-  static String selectThisRadioButton(int e, ScopedAnxiety model) {
+  static TodayWas selectThisRadioButton(int e, ScopedAnxiety model) {
 
     switch (e) {
       case 1:
         model.updateGradientColor(1);
-        return TodayWas.Awesome.toString();
+        return TodayWas.Awesome;
         break;
       case 2:
         model.updateGradientColor(2);
-        return TodayWas.Good.toString();
+        return TodayWas.Good;
         break;
       case 3:
         model.updateGradientColor(3);
-        return TodayWas.Fine.toString();
+        return TodayWas.Fine;
         break;
       case 4:
         model.updateGradientColor(4);
-        return TodayWas.NotSoGood.toString();
+        return TodayWas.NotSoGood;
         break;
       case 5:
         model.updateGradientColor(5);
-        return TodayWas.Terrible.toString();
+        return TodayWas.Terrible;
         break;
       default:
         model.updateGradientColor(0);
-        return TodayWas.NotSelectedYet.toString();
+        return TodayWas.NotSelectedYet;
         break;
     }
   }
