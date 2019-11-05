6ae704bcb59305fc91926e3694094657370d7947
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2019-11-05 11:48:36 -0600

    Initializing data to [] instead of null, created 2 nw member variables anxEntryController, and readOnly.

--- lib/models/anxiety_model/anxiety_baby_class.dart
+++ lib/models/anxiety_model/anxiety_baby_class.dart
@@ -1,8 +1,15 @@
 ///This is a very small class who's sole purpose is to be used with the scoped_model library
 ///to be monitored for when is items are updated.
 ///This particular one is meant to be used with the anxiety model.
+
+import 'package:flutter/material.dart';
+
 class AnxietyBabyClass {
-  var data = null;
+  //Will contain the data frm the database.
+  var data = [];
   //Sets the initial color to be the green gradient.
   int groupValue = 1;
+  //The controller adds additional functionality and data retrieval to the TextFormField().
+  final TextEditingController anxEntryController = TextEditingController();
+  final TextEditingController readOnly = TextEditingController();
 }
